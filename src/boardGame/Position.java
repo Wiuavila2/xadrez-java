d59d046c36dad130b1eeb841bc9c4c11eeb7fbfe
Wiuavila2 <wiuneivetom.avila@outlook.com>
2024-06-23 21:16:43 -0300

    classe posicao criada

---
+++ src/boardGame/Position.java
@@ -0,0 +1,29 @@
+package boardGame;
+
+public class Position {
+	private int Linha;
+	private int coluna;
+	public Position(int linha, int coluna) {
+		super();
+		Linha = linha;
+		this.coluna = coluna;
+	}
+	public int getLinha() {
+		return Linha;
+	}
+	public void setLinha(int linha) {
+		Linha = linha;
+	}
+	public int getColuna() {
+		return coluna;
+	}
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+	@Override
+	public String toString() {
+		return Linha+ ", " + coluna;
+	}
+	
+	
+}
