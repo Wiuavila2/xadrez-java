3f436bc4f5a82bab8c77749648ff1f3eab75d107
Wiuavila2 <wiuneivetom.avila@outlook.com>
2024-06-29 17:18:51 -0300

    melhoria na impressão do tabuleiro

--- src/Xadrez/PartidaDeXadrez.java
+++ src/Xadrez/PartidaDeXadrez.java
@@ -29,9 +29,19 @@ public class PartidaDeXadrez {
 	}
 	
 	private void setupInicial() {
-		ColocarNovaPeca( 'b' , 6, new Torre(tabuleiro, Cor.BRANCO));
-		ColocarNovaPeca( 'e', 8, new Rei(tabuleiro, Cor.PRETO));
-		ColocarNovaPeca( 'e', 1,new Rei(tabuleiro, Cor.BRANCO));
+		ColocarNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+		ColocarNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+		ColocarNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+		ColocarNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+		ColocarNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+        ColocarNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+        ColocarNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+        ColocarNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+        ColocarNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+        ColocarNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+        ColocarNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+        ColocarNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
 	}
 	
 	
